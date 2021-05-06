e875f5f32a203697cd796dc695d0888a09edb718
Vaibhav Kushwaha <vaibhav20092000@gmail.com>
2021-05-06 18:52:55 +0530

    search by district page completed, decoration needed

--- lib/pages/searchByDistrict.dart
+++ lib/pages/searchByDistrict.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
@@ -7,14 +9,76 @@ class SearchByDistrict extends StatefulWidget {
 }
 
 class _SearchByDistrictState extends State<SearchByDistrict> {
+  // variables for states
   List<String> stateList;
-  Map<String, int> stateId;
-  var _chosenStateId;
+  Map<String, String> stateId = {};
+  String _chosenStateId;
+
+  // variables for districts
+  List<String> districtList;
+  Map<String, String> districtId = {};
+  String _chosenDistrictId;
+
+  final String userAgentHeaderForApi = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.51';
   final String stateApiRequest = 'https://cdn-api.co-vin.in/api/v2/admin/location/states';
+  final String districtApiRequest = 'https://cdn-api.co-vin.in/api/v2/admin/location/districts/';
   Future<void> getStateList() async {
-    Response response = await get(Uri.parse(stateApiRequest));
-    print('Response status code: ${response.statusCode}');
+    try {
+      Response response = await get(
+        Uri.parse(stateApiRequest),
+        headers: {'User-Agent': userAgentHeaderForApi},
+      );
+      if(response.statusCode < 400) {
+        dynamic data = jsonDecode(response.body);
+        var stateData = data['states'];
+        List<String> tempList = [];
+        Map<String, String> tempDict = {};
+        stateData.forEach((item) {
+          tempList.add(item['state_name']);
+          tempDict[item['state_name']] = item['state_id'].toString();
+        });
+
+        setState(() {
+          stateList = tempList;
+          stateId = tempDict;
+        });
+      } else {
+        print('Reponse returned with error code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
   }
+
+  Future<void> getDistrictsList(String stateId) async {
+    try {
+      Response response = await get(
+        Uri.parse(districtApiRequest + stateId),
+        headers: {'User-Agent':userAgentHeaderForApi},
+      );
+      if(response.statusCode < 400) {
+        dynamic data = jsonDecode(response.body);
+        var districtData = data['districts'];
+        List<String> tempList = [];
+        Map<String, String> tempDict = {};
+
+        districtData.forEach((item) {
+          tempList.add(item['district_name']);
+          tempDict[item['district_name']] = item['district_id'].toString();
+        });
+
+        setState(() {
+          districtList = tempList;
+          districtId = tempDict;
+        });
+      } else {
+        print('Response returned with error code: ${response.statusCode}');
+      }
+    } catch(e) {
+      print(e.toString());
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -29,20 +93,77 @@ class _SearchByDistrictState extends State<SearchByDistrict> {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             (stateList == null) ? CircularProgressIndicator() :
                 DropdownButton<String>(
+                  icon: Icon(Icons.keyboard_arrow_down, color: Colors.blue,),
+                  // TODO: add meaningful Strings to hint and disabled hint
+                  hint: Text('Select a state'),
+                  disabledHint: Text('Disabled hint'),
                   value: _chosenStateId,
                   items: stateList.map<DropdownMenuItem<String>>((String stateName){
                     return DropdownMenuItem<String>(
-                      value: stateId[stateName].toString(),
+                      value: stateId[stateName],
                       child: Text(stateName),
                     );
-                  }),
+                  }).toList(),
+                  onChanged: (String val) async {
+                    // val is the state Id of the selection
+                    getDistrictsList(val);
+                    setState(() {
+                      _chosenStateId = val;
+                    });
+                  },
                 ),
+            SizedBox(height: 20,),
+            (districtList == null) ? SizedBox(height: 5) :
+            DropdownButton<String>(
+              icon: Icon(Icons.keyboard_arrow_down, color: Colors.blue,),
+              hint: Text('Select district'),
+              disabledHint: Text('Select a state first'),
+              value: _chosenDistrictId,
+              items: districtList.map<DropdownMenuItem<String>>((String districtName){
+                return DropdownMenuItem<String>(
+                  value: districtId[districtName],
+                  child: Text(districtName),
+                );
+              }).toList(),
+              onChanged: (String val) {
+                setState(() {
+                  _chosenDistrictId = val;
+                });
+              },
+            ),
+            SizedBox(height: 20,),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.pinkAccent[100],
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: TextButton(
+                child: Text('Search', style: TextStyle(color: Colors.black),),
+                onPressed: () {
+                  // TODO: add search by district function
+                  // if no state is selected then add a response for that too
+                  openSnackBar(context, 'Search function coming soon!');
+                },
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+void openSnackBar(BuildContext context, String str)
+{
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(str),
+      behavior: SnackBarBehavior.floating,
+      duration: Duration(seconds: 1),
+    ),
+  );
 }
