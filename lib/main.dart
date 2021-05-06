3f33fb2ba1c8dc31251f10459640cff91e5bea35
Vaibhav Kushwaha <vaibhav20092000@gmail.com>
2021-05-06 22:43:21 +0530

    neumorphism theme updated

--- lib/main.dart
+++ lib/main.dart
@@ -16,39 +16,101 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffab9adf),
       appBar: AppBar(
-        title: Text('Vaccine Slots'),
+        title: Text('Vaccine Slot Checker'),
+        backgroundColor: Color(0xffab9adf),
+        elevation: 0,
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              child: Text("Search By PIN"),
-              onPressed: (){
-                openSnackBar(context, 'Coming soon!');
-              },
-            ),
-            ElevatedButton(
-              child: Text("Search By District"),
-              onPressed: (){
-                Navigator.pushNamed(context, '/searchByDistrict');
-              },
-            ),
-            ElevatedButton(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Notify Me!'),
-                  Icon(
-                    Icons.lightbulb_outline,
-                  )
-                ],
+            Container(
+              // width: (MediaQuery.of(context).size.width)/3,
+              decoration: BoxDecoration(
+                color: Color(0xffab9adf),
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xff897baf),
+                    offset: Offset(1, 1),
+                    blurRadius: 3,
+                    spreadRadius: 3,
+                  ),
+                  BoxShadow(
+                    color: Color(0xffcdb9ff),
+                    offset: Offset(-1, -1),
+                    blurRadius: 3,
+                    spreadRadius: 2,
+                  ),
+                ]
               ),
-              onPressed: (){
-                openSnackBar(context, 'Coming soon!');
-              },
+              child: TextButton(
+                child: Text(
+                  "Search By PIN",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black45,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                onPressed: (){
+                  openSnackBar(context, 'Coming soon!');
+                },
+              ),
             ),
+            SizedBox(height: 20,),
+            Container(
+              // width: (MediaQuery.of(context).size.width)/3,
+              decoration: BoxDecoration(
+                  color: Color(0xffab9adf),
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xff897baf),
+                      offset: Offset(1, 1),
+                      blurRadius: 3,
+                      spreadRadius: 3,
+                    ),
+                    BoxShadow(
+                      color: Color(0xffcdb9ff),
+                      offset: Offset(-1, -1),
+                      blurRadius: 3,
+                      spreadRadius: 2,
+                    ),
+                  ],
+              ),
+              child: TextButton(
+                child: Text(
+                  "Search By District",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black45,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                onPressed: (){
+                  Navigator.pushNamed(context, '/searchByDistrict');
+                },
+              ),
+            ),
+
+            // TODO: uncomment the following button to enable NOTIFY ME
+            // ElevatedButton(
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       Text('Notify Me!'),
+            //       Icon(
+            //         Icons.lightbulb_outline,
+            //       )
+            //     ],
+            //   ),
+            //   onPressed: (){
+            //     openSnackBar(context, 'Coming soon!');
+            //   },
+            // ),
           ],
         ),
       ),
